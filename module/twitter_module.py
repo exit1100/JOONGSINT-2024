b0d3dea6fd7d678aee06bd242a8fab4e236b8681
nms200299 <nms200299@naver.com>
2024-04-21 03:44:36 +0900

    Twitter 모듈 수정

--- module/twitter_module.py
+++ module/twitter_module.py
@@ -1,96 +1,118 @@
 
 from flask import Blueprint, render_template, request
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+# pip install webdriver-manager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-import time, re
+import time
+import sys
+from config import Twitter_ID, Twitter_PW
+
 
 twitter_module = Blueprint("twitter_module", __name__)
 
 @twitter_module.route("/twitter_result", methods=["POST"])
 def twitter_result():
     class SNSProfileScraper:
-        def __init__(self, username , driver_path):
-            self.driver_path = driver_path
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('--disable-extensions')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(executable_path='app/chromedriver.exe', options=options)
+        
+        def __init__(self, username):
             self.username = username
 
+        def login_twitter(self, login_name, login_pw):
+            self.driver.get('https://twitter.com/i/flow/login')
+            time.sleep(3)
+            self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input').send_keys(login_name)
+            # ID 또는 이메일 주소 입력
+            self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div').click()
+            # 확인 버튼 클릭
+            time.sleep(3)
+            message = self.driver.find_element(By.XPATH, '//*[@id="modal-header"]/span/span').text
+            if message == '휴대폰 번호 또는 사용자 아이디 입력':
+            # 비정상적인 로그인 다수 시도 예외 처리
+                self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input').send_keys(login_name)
+                # ID 또는 전화번호 입력
+                self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div').click()
+                # 확인 버튼 클릭
+                time.sleep(3)    
+            self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input').send_keys(login_pw)
+            # 패스워드 입력
+            self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div').click()
+            # 확인 버튼 클릭
+            time.sleep(3)
+            
         def scrape_twitter_profile(self):
+            self.driver.get('https://twitter.com/'+self.username)
+            time.sleep(3)
+        
+            # [*] 트위터 프로필 구조
+            #   // 프로필 이미지 (고정)
+            #   // 아이디 (고정)
+            #   // 닉네임 (고정)
+            #   // SUMMARY (유동)
+            #   // 웹 사이트 주소 (유동)
+            #   // 위치 (유동)
+            #   // 생년월일 (유동)
+            #   // 최초 가입일 (고정)
+            
+            profile_img = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[1]/div[1]/div[2]/div/div[2]/div/a/div[3]/div/div[2]/div/img').get_property('src')
+            name = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div[1]/div/div[1]/div/div/span/span[1]').text
+            id = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div[1]/div/div[2]/div/div/div/span').text
+            summary= ''
+            location = ''
+            joined_date = ''
+            propertyCou = 1
+            
             try:
-                url = 'https://twitter.com/' + self.username
-                options = webdriver.ChromeOptions()
-                options.add_argument('headless')
-                options.add_argument('--disable-extensions')
-                options.add_argument('--disable-gpu')
-                options.add_argument('--no-sandbox')
-                driver = webdriver.Chrome(executable_path=self.driver_path, options=options)
-                driver.get(url)
-                time.sleep(5)
-                try:                
-                    name = driver.find_element(By.CSS_SELECTOR , '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div:nth-child(1) > div > div > div > div > div > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1777fci > div > h2 > div > div > div > div > span.css-901oao.css-16my406.r-1awozwy.r-18jsvk2.r-6koalj.r-poiln3.r-b88u0q.r-bcqeeo.r-1udh08x.r-3s2u2q.r-qvutc0 > span > span:nth-child(1)')
-                    name = name.text
-                except:
-                    name = None
-
-                try:
-                    screen_name = driver.find_element(By.CSS_SELECTOR , '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div.css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv > div.css-1dbjc4n.r-6gpygo.r-14gqq1x > div.css-1dbjc4n.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1wbh5a2 > div > div > div > span')
-                    screen_name = screen_name.text
-                except:
-                    screen_name = 'None'
-
-                try:
-                    bio = driver.find_element(By.CSS_SELECTOR , '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div.css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv > div:nth-child(3) > div > div > span')
-                    bio = bio.text
-                except:
-                    bio = None
-
-                try:
-                    location = driver.find_element(By.CSS_SELECTOR , '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div.css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv > div:nth-child(4) > div > span:nth-child(1) > span > span')
-                    location = location.text
-                except:
-                    location = None
-
-                try:
-                    profile_img = driver.find_element(By.CSS_SELECTOR , '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div.css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv > div.css-1dbjc4n.r-1habvwh.r-18u37iz.r-1w6e6rj.r-1wtj0ep > div.css-1dbjc4n.r-1adg3ll.r-16l9doz.r-6gpygo.r-2o1y69.r-1v6e3re.r-bztko3.r-1xce0ei > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > a > div.css-1dbjc4n.r-14lw9ot.r-sdzlij.r-1wyvozj.r-1udh08x.r-633pao.r-u8s1d.r-1v2oles.r-desppf > div > div.r-1p0dtai.r-1pi2tsx.r-1d2f490.r-u8s1d.r-ipm5af.r-13qz1uu > div > img')
-                    profile_img = profile_img.get_attribute('src')
-                except:
-                    url = None
-
-                try:
-                    joined_date = driver.find_element(By.CSS_SELECTOR , '#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div:nth-child(3) > div > div > div > div.css-1dbjc4n.r-1ifxtd0.r-ymttw5.r-ttdzmv > div:nth-child(4) > div > span.css-901oao.css-16my406.r-14j79pv.r-4qtqp9.r-poiln3.r-1b7u577.r-bcqeeo.r-qvutc0 > span')
-                    joined_date = joined_date.text
-                except:
-                    joined_date = None
-
-                try:
-                    profile_data = {
-                        'sns' : 'twitter',
-                        'name': name,
-                        'screen_name': screen_name,
-                        'bio': bio,
-                        'location': location,
-                        'profile_img': profile_img,
-                        'joined_date': joined_date
-                    }
-                    return profile_data
-                except:
-                    return None
+                summary = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[3]/div/div/span').text
+                locTestId = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[4]/div/span['+str(propertyCou)+']'
             except:
-                return None
+                locTestId = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[3]/div/span['+str(propertyCou)+']'
+                # summary가 없으면 xPath 상 div 위치가 1 작아짐.
+                pass
+            
+            while(True):
+                try:
+                    propertyTestId = self.driver.find_element(By.XPATH, locTestId).get_attribute('data-testid')
+                    if propertyTestId == 'UserLocation': # 위치 정보
+                        location = self.driver.find_element(By.XPATH, locTestId+'/span/span').text
+                    elif propertyTestId == 'UserJoinDate': # 최초 가입일 정보
+                        joined_date = self.driver.find_element(By.XPATH, locTestId+'/span').text
+                    elif (propertyTestId == '') or (propertyTestId == None):
+                        break
+                    propertyCou += 1
+                    locTestId = locTestId.replace('span['+str(propertyCou-1)+']','span['+str(propertyCou)+']')
+                    # 다음 순회를 위한 xPath 수정
+                except:
+                    break
 
-    
-    
-    driver_path = 'app/chromedriver.exe'
+            profile_data = {
+                'sns' : 'twitter',
+                'name': id,
+                'screen_name': name,
+                'bio': summary,
+                'location': location,
+                'profile_img': profile_img,
+                'joined_date': joined_date
+            }
+            print(profile_data)
+            return profile_data
 
     find_name = request.cookies.get("NAME")
 
-    scraper = SNSProfileScraper(find_name , driver_path)
+    scraper = SNSProfileScraper(find_name)
+    scraper.login_twitter(Twitter_ID, Twitter_PW)
     twitter_profile = scraper.scrape_twitter_profile()
 
     result ={}
 
     result['twitter'] = twitter_profile
-    
+
     return render_template("twitter_result.html", result=result)
