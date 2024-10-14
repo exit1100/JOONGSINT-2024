622a0139e0d27d7450e38fd175daa9404f89a2fd
exit1100 <85146195+exit1100@users.noreply.github.com>
2024-10-14 23:28:30 +0900

    [YEJUN] Code Update

--- module/db_module.py
+++ module/db_module.py
@@ -7,77 +7,57 @@ def init(host,port,user,password,db):
     mysql_user =  os.environ.get('MYSQL_USER', user)
     mysql_password = os.environ.get('MYSQL_PASSWORD', password)
     mysql_db = os.environ.get('MYSQL_DB', db)
-
     db = pymysql.connect(host=mysql_host,port=mysql_port, user=mysql_user, password=mysql_password, db=mysql_db, charset='utf8')
-
     return db
 
 
 def insert(db, module, type, json_result, user):
-
     cursor = db.cursor()
-
     cursor.execute("INSERT INTO result (module, type, result, user) VALUES (%s, %s, %s, %s)", (module, type, json_result, user))
-
     db.commit()
     #db.close()
 
 
 def user_update(db,field,value,user):
     cursor = db.cursor()
-
     query = "UPDATE user_detail SET {} = %s WHERE user_id = %s".format(field)
     cursor.execute(query, (value, user))
-
     db.commit()
     #db.close()
 
 
 def get_setting(db,field,user):
     cursor = db.cursor()
-
     # query = "SELECT %s FROM user_detail WHERE user_id = %s"
     # cursor.execute(query, (field, user))
-
     cursor.execute("select " + field + " from user_detail WHERE user_id = %s", (user,))
-    
     data = cursor.fetchone()
-
     db.commit()
     #db.close()
-
     return data[0]
+
+
 def get_result(db,user):
     cursor = db.cursor()
-
     cursor.execute("select module,date from result WHERE user = %s", (user))
-    
     data = cursor.fetchall()
-
     db.commit()
     db.close()
-
     return data
+
 
 def get_only_result(db,user,module,date):
     cursor = db.cursor()
-
-
     cursor.execute("select result from result WHERE user = %s and module = %s and date = %s", (user,module,date))
-    
     data = cursor.fetchone()
-
     db.commit()
     db.close()
-
     return data[0]
-    
-# config set
 
+
+# config set
 # host = '127.0.0.1'
 # port = 3306
 # user = 'root'
 # password = ''
 # db = 'joongsint'
-
-
